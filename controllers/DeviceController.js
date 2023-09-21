e866d910e7025c0fc44a16cc7db41b54a62d506b
Croyant265 <99621511+Croyant265@users.noreply.github.com>
2023-09-22 00:37:38 +0200

    Update DeviceController.js

--- controllers/DeviceController.js
+++ controllers/DeviceController.js
@@ -1,9 +1,9 @@
-const Device = require('../models/device')
+const device = require('../models/device')
 
 async function createDevice(req, res, next) {
   try {
     const { application_id, device_name, latitude, longitude, altitude } = req.body;
-    const deviceId = await Device.createDevice({ application_id, device_name, latitude, longitude, altitude })
+    const deviceId = await device.createDevice({ application_id, device_name, latitude, longitude, altitude })
     res.status(201).json({ deviceId })
   } catch (error) {
     next(error)
