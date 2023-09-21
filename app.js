15fc4c84157280c1549b97003712d121ea1023e3
Croyant265 <99621511+Croyant265@users.noreply.github.com>
2023-09-21 22:24:09 +0200

    Update app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const deviceRoutes = require('./routes/deviceRoutes')
+const recordRoutes = require('./routes/recordRoutes')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+
+app.use('/api', deviceRoutes)
+app.use('/webhook', recordRoutes)
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something went wrong!')
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
