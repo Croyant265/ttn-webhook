fe01c79233b387ae5981f1f8293f9de07ecd8427
Croyant265 <99621511+Croyant265@users.noreply.github.com>
2023-09-21 23:25:55 +0200

    Create deviceRoutes.js

---
+++ routes/deviceRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+const deviceController = require('../controllers/deviceController')
+
+
+router.post('/devices', deviceController.createDevice)
+
+module.exports = router
