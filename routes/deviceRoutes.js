cc9b806c8e947c7cb00e21a945ad8ce2e5b74fbd
Believer <99621511+Croyant265@users.noreply.github.com>
2023-09-22 17:09:57 +0200

    Update deviceRoutes.js

--- routes/deviceRoutes.js
+++ routes/deviceRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const deviceController = require('../controllers/deviceController') 
 const auth = require('../middleware/auth')
 
-
 router.post('/devices', auth, deviceController.createDevice)
 
 module.exports = router
