03950120684905cf096717227af66a83727089a2
Croyant265 <99621511+Croyant265@users.noreply.github.com>
2023-09-22 16:55:49 +0200

    Update recordRoutes.js

--- routes/recordRoutes.js
+++ routes/recordRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const recordController = require('../controllers/recordController')
+const auth = require('../middleware/auth')
 
-router.post('/post-data', recordController.createDataRecord)
+router.post('/post-data', auth, recordController.createDataRecord)
 
 module.exports = router
